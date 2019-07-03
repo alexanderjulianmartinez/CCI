7efd680581ec00a4eb338e5b628052fee71dbef8
alexanderjulianmartinez <32147165+alexanderjulianmartinez@users.noreply.github.com>
2019-07-03 16:35:53 -0700

    added black jack card class

--- Object-Oriented Design/DeckOfCards/black_jack_card.py
+++ Object-Oriented Design/DeckOfCards/black_jack_card.py
@@ -2,4 +2,22 @@ from DeckOfCards.card import Card
 
 
 class BlackJackCard(Card):
-    ...
+    def is_face_card(self):
+        return (self.faceValue >= 11) and (self.faceValue <= 13)
+
+    def value(self):
+        if self.is_ace():
+            return 1
+        elif self.is_face_card():
+            return 10
+        else:
+            return self.faceValue
+
+    def is_ace(self):
+        return self.faceValue == 1
+
+    def min_value(self):
+        if self.is_ace():
+            return 1
+        else:
+            return self.value()
